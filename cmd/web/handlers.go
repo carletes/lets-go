ffcf23cec393ffae109d6308ddc99387752d237f
Carlos Valiente <carlos@pepelabs.net>
2018-04-16 18:42:03 +0100

    HTML templates.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,9 +2,37 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 	"strconv"
 )
+
+// Helper function, copied from Go's standard library. The original
+// expects a list of file names as arguments, whereas here we process
+// a list of template contents.
+func parseTemplates() (*template.Template, error) {
+	var t *template.Template
+	var err error
+	for path, src := range templates {
+		name := filepath.Base(path)
+		var tmpl *template.Template
+		if t == nil {
+			t = template.New(name)
+		}
+		if name == t.Name() {
+			tmpl = t
+		} else {
+			tmpl = t.New(name)
+		}
+		_, err = tmpl.Parse(src)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return t, nil
+}
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -12,7 +40,19 @@ func Home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write([]byte("Hello from Snippetbox"))
+	ts, err := parseTemplates()
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
 }
 
 func NewSnippet(w http.ResponseWriter, r *http.Request) {
