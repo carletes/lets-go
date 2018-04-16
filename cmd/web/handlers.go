a3751debc3f42469ee91fd5187ceb41ba99376bb
Carlos Valiente <carlos@pepelabs.net>
2018-04-16 13:28:19 +0100

    Extra handlers.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +13,18 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func NewSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display the new snippet form"))
+}
+
+func ShowSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Snippet: %d", id)
 }
