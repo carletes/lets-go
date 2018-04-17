caf7a3c8f26ca7376faa08b7a9b81206d34edc34
Carlos Valiente <carlos@pepelabs.net>
2018-04-17 20:43:19 +0100

    Configurable patameters.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP listen address")
+	staticDir := flag.String("static-dir", "ui/static", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/snippet", ShowSnippet)
 	mux.HandleFunc("/snippet/new", NewSnippet)
 
-	fileServer := http.FileServer(http.Dir("ui/static"))
+	fileServer := http.FileServer(http.Dir(*staticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
